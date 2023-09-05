80548534d71054149bb1d29aa6c1b1b6fc463e5e
devTylr <tyler.ward1199@gmail.com>
2023-09-05 08:46:36 -0400

    Added operator functions and simple ui skeleton

---
+++ script.js
@@ -0,0 +1,37 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+let firstNumber;
+let SecondNumber;
+let operator;
+
+const operate = function(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
+}
+console.log(operate('-', 60, 6))
